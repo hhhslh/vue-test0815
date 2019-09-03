087e153748a16be44d696ad32370ae22e42048af
hhhslh <3064063267@qq.com>
2019-09-03 15:24:03 +0800

    s0903

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,34 +5,66 @@
         <button @click="reduce(10)">-</button>
         <button @click="yadd(15)">异步+</button>
         <button @click="yreduce(20)">异步-</button>
-        <router-link to="/hi">跳hi</router-link>
+        <router-link to="/hi">跳子</router-link>
+        <hi :message="parentMsg.total" @sendMsgtoParent="msgFromChild"></hi>
     </div>
 </template>
 
 <script>
+import hi from "./hi"
 import {getSchool} from "../api/api"
 export default {
+    components:{
+        hi
+    },
   data () {
     return {
-     
+        parentMsg:"",
+        points:0,
+        bet:{
+            pointsec:50,
+            pointHisytoy:"local"
+        }
     }
   },
+  computed: {
+      pointHisytoy(){
+          return this.bet.pointHisytoy
+      }
+  },
+  watch:{
+      points(newValue,index){
+          console.log(newValue+"---"+index)
+      },
+      bet:{
+          handler(s,index){
+              console.log(s+"---"+index)
+          },
+          deep:true
+      }
+  },
   methods: {
+    //   子传父
+      msgFromChild(child){
+          console.log(child)
+      },
      apiTest(){
          getSchool({
             pageNum: 1,
             pageSize: 24
          }).then(res=>{
-             console.log(res)
+            this.parentMsg=res.data
          })
      },
      add(n){
+         this.points++
          this.$store.commit('mutationsAddCount',n);
      },
      reduce(n){
           this.$store.commit('mutationsReduceCount',n);
      },
      yadd(n){
+         this.bet.pointsec++
          this.$store.dispatch('actionsAddCount',n)
      },
      yreduce(n){
--- src/components/hi.vue
+++ src/components/hi.vue
@@ -1,12 +1,22 @@
 <template>
     <div>
-        <h1>哈哈哈哈哈哈</h1>
+        <h1>子</h1>
+        <p>{{message}}</p>
+        <button @click="sendMsgtoParent">向父组件传值</button>
     </div>
 </template>
 
 <script>
 export default {
-
+    // 父传子
+    props:["message"],
+    methods:{
+        sendMsgtoParent(){
+            // 子传父
+            this.$emit("sendMsgtoParent","this message from children")
+             console.log(this.message)
+        }
+    }
 }
 </script>
 
